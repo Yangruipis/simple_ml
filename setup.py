cdbc75fde28e45130a655a7dc0d495b8b675c1b9
yangruipis <yangruipis@163.com>
2018-04-21 20:42:36 +0800

    future add faild

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setuptools比distutils.core多了检查包依赖功能
 """
 
 
-if sys.version_info < (2,7):
-    sys.exit('Sorry, Python < 2.7 is not supported')
+if sys.version_info < (3,5):
+    sys.exit('Sorry, Python < 3.5 is not supported')
 
 
 setup(
@@ -22,6 +22,6 @@ setup(
 
     url="https://github.com/Yangruipis/simple_ml",
     packages=['simple_ml', 'simple_ml.base'],
-    install_requires=['enum34 >1.0.0', 'numpy>=1.10', 'setuptools>=16.0', 'matplotlib>2.0.0', 'scipy>0.15.0', 'requests>2.10'],
+    install_requires=['numpy>=1.10', 'setuptools>=16.0', 'matplotlib>2.0.0', 'scipy>0.15.0', 'requests>2.10'],
     # dependency_links = ['https://github.com/minepy/minepy/archive/1.2.2.tar.gz#egg=minepy-1.2.2']
 )

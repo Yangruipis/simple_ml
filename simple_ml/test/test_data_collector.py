c129cb0bb926122728713979409b5ab92e90c98a
yangruipis <yangruipis@163.com>
2018-04-23 17:41:32 +0800

    test get_iris

--- simple_ml/test/test_data_collector.py
+++ simple_ml/test/test_data_collector.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
 
 import unittest
-from simple_ml.classify_data import DataCollector
+from simple_ml.classify_data import DataCollector, get_iris
 import numpy as np
+
 
 class TestDataCollector(unittest.TestCase):
 
@@ -12,6 +13,12 @@ class TestDataCollector(unittest.TestCase):
         self.assertIsInstance(x, np.ndarray)
         self.assertEqual(x.shape[0], 150)
         self.assertEqual(x.shape[1], 6)
+
+    def test_build_in_get_iris(self):
+        x, y = get_iris()
+        self.assertEqual(len(x.shape), 2)
+        self.assertEqual(len(y.shape), 1)
+
 
 if __name__ == '__main__':
     unittest.main()

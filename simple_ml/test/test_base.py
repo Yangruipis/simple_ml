c966370ffec87be87e890a0b94892c67c824af56
yangruipis <yangruipis@163.com>
2018-06-21 20:55:45 +0800

    add test base model

--- simple_ml/test/test_base.py
+++ simple_ml/test/test_base.py
@@ -4,9 +4,7 @@ import unittest
 from simple_ml.base.base_model import *
 import numpy as np
 from numpy.testing import assert_array_equal
-from numpy.testing import assert_array_almost_equal
 from simple_ml.base.base_error import *
-
 
 
 class TestBaseModel(unittest.TestCase):
